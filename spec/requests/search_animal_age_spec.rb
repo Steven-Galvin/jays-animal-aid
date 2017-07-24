8dbe55d40768c584c7c81e4ccfd6234fc37adec8
Steven Galvin <steven.john.galvin@gmail.com>
2017-07-24 08:39:57 -0700

    test and pass searching by age and breed

---
+++ spec/requests/search_animal_age_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "search animals by age route", type: :request do
+
+  before do
+    @animal = FactoryGirl.create(:animal)
+    get "/animals?age=#{@animal.age}"
+  end
+
+  it 'returns animal matching age' do
+    expect(jsonParse[0]['name']).to eq(@animal.name)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
---
+++ spec/requests/search_animal_breed_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "search animals by breed route", type: :request do
+
+  before do
+    @animal = FactoryGirl.create(:animal)
+    get "/animals?breed=#{@animal.breed}"
+  end
+
+  it 'returns animal matching breed' do
+    expect(jsonParse[0]['name']).to eq(@animal.name)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
