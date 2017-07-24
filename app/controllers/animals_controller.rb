e952cab34226d1e0fd3eaf71ecca42c16c506e9c
Steven Galvin <steven.john.galvin@gmail.com>
2017-07-24 08:30:20 -0700

    add scopes for searching by age and searching by breed

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,44 +1,50 @@
 class AnimalsController < ApplicationController
   include Response
   def index
+    if age = params[:age]
+      @animals = Animal.age_search(age)
+    elsif breed = params[:breed]
+      @animals = Animal.breed_search(breed)
+    else
       @animals = Animal.all
-      json_response(@animals)
     end
+    json_response(@animals)
+  end
 
-    def show
-      @animal = Animal.find(params[:id])
-      json_response(@animal)
-    end
+  def show
+    @animal = Animal.find(params[:id])
+    json_response(@animal)
+  end
 
-    def create
-      if @animal = Animal.create!(animal_params)
-        render status: 201, json: {
-           message: "Your animal has been created successfully."
-        }
-      end
-    end
-
-    def update
-      @animal = Animal.find(params[:id])
-      if @animal.update!(animal_params)
-        render status: 200, json: {
-           message: "Your animal has been updated successfully."
-        }
-      end
-    end
-
-    def destroy
-      @animal = Animal.find(params[:id])
-      if @animal.destroy
-        render status: 202, json: {
-           message: "Your animal has been destroyed successfully."
-        }
-      end
-    end
-
-    private
-
-    def animal_params
-      params.permit(:specie, :breed, :name, :age, :bio, :likes, :dislikes, :picture)
+  def create
+    if @animal = Animal.create!(animal_params)
+      render status: 201, json: {
+         message: "Your animal has been created successfully."
+      }
     end
   end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update!(animal_params)
+      render status: 200, json: {
+         message: "Your animal has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    if @animal.destroy
+      render status: 202, json: {
+         message: "Your animal has been destroyed successfully."
+      }
+    end
+  end
+
+  private
+
+  def animal_params
+    params.permit(:specie, :breed, :name, :age, :bio, :likes, :dislikes, :picture)
+  end
+end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,6 @@
 class Animal < ApplicationRecord
   validates :specie, :breed, :name, :age, :bio, presence: true
+
+  scope :age_search, -> (input) { where("age = ?", input.to_i)}
+  scope :breed_search, -> (input) { where("breed like ?", "#{input}")}
 end
